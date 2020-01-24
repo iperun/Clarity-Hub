d7548f85bb6c2ba19e682246a2bdeae6743d753f
Ivan Perun <peruni101@gmail.com>
2020-01-24 16:46:26 -0500

    pages updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
 import React from "react"
+import {Link} from 'gatsby'
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Clarity Hub</h1>
+            <h2>A collection of online tools for creative developers.</h2>
+            <h3>
+                <Link to="/contact">Contact</Link>
+            </h3>
+        </div>
+    )
+}
+
+export default IndexPage
