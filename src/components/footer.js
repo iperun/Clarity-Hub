475a156fd3f6a12331c1d293499a76b0ca702932
Ivan Perun <peruni101@gmail.com>
2020-02-14 22:34:41 -0500

    link updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import {Link} from 'gatsby'
 
 import footerStyles from './footer.module.scss'
 
 const Footer = () => {
     return (
         <footer>
-            <p> <a className={footerStyles.social} href="https://www.behance.net/peruni101cbb7">Behance</a>
-                <a className={footerStyles.social} href="https://dribbble.com/peruni">Dribbble</a>
-                 <a className={footerStyles.social} href="https://github.com/iperun">Github</a>
-                <a className={footerStyles.social} href="https://www.instagram.com/perundesign/">Instagram</a>
+            <p> <a className={footerStyles.social} href="https://www.behance.net/peruni101cbb7" target="_blank">Behance</a>
+                <a className={footerStyles.social} href="https://dribbble.com/peruni" target="_blank">Dribbble</a>
+                 <a className={footerStyles.social} href="https://github.com/iperun" target="_blank">Github</a>
+                <a className={footerStyles.social} href="https://www.instagram.com/perundesign/" target="_blank">Instagram</a>
             </p>
-            <p className={footerStyles.copyright}>Created by <a className={footerStyles.pdesign}href="htts://perundesign.com"> Ivan Perun</a>, Clarity © 2020</p>
+            <p className={footerStyles.copyright}>Created by <a className={footerStyles.pdesign} href="https://perundesign.com" target="_blank"> Ivan Perun</a>, Clarity © 2020</p>
 
         </footer>
     )
